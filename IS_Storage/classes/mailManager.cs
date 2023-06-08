106eb2667b32fd2fcad73a28d2b15f04c3790a74
Григорий Асонов <grasonof@gmail.com>
2023-06-08 07:10:38 +0500

    jk

--- IS_Storage/classes/mailManager.cs
+++ IS_Storage/classes/mailManager.cs
@@ -13,21 +13,25 @@ namespace IS_Storage.classes
     {
         static public async void Sending(MailMessage text, string file = null)
         {
-            if (file != null)
+            try
             {
-                text.Attachments.Add(new Attachment(file));
+                if (file != null)
+                {
+                    text.Attachments.Add(new Attachment(file));
+                }
+                // отправитель - устанавливаем адрес и отображаемое в письме имя
+                SmtpClient smtp = new SmtpClient()
+                {
+                    Port = 587,
+                    Host = "smtp.rambler.ru",
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential("is_storage@rambler.ru", "115544Gg"),
+                    EnableSsl = false,
+                    DeliveryMethod = SmtpDeliveryMethod.Network
+                };
+                await smtp.SendMailAsync(text);
             }
-            // отправитель - устанавливаем адрес и отображаемое в письме имя
-            SmtpClient smtp = new SmtpClient()
-            {
-                Port = 587,
-                Host = "smtp.rambler.ru",
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential("is_storage@rambler.ru", "115544Gg"),
-                EnableSsl = false,
-                DeliveryMethod = SmtpDeliveryMethod.Network
-            };
-            await smtp.SendMailAsync(text);
+            catch { }
 
         }
     }
